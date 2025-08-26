3912c879f70e4d95e0ae9b8298835c1bed7d4085
frontback333 <gunhu2007@gmail.com>
2025-08-26 22:50:08 +0900

    Duplicated Folder Fix

--- build/Desktop_Qt_6_9_1_MinGW_64-bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_System_FILE D:/TB-3582/System/build/Desktop_Qt_6_9_1_MinGW_64-bit-Debug/System.exe)
-set(__QT_DEPLOY_TARGET_System_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_System_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
